4a33a87c0ce2909abce45eb42edfc61c08560015
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2025-09-26 14:35:59 +0400

    add vercel.json & update Cart Item

--- src/features/Cart/components/CartItemCard.tsx
+++ src/features/Cart/components/CartItemCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useCartStore, type CartItem } from "@/features/Cart/store/cart";
@@ -13,22 +15,26 @@ export default function CartItemCard({ item }: Props) {
   return (
     <Card className="bg-pure-white shadow-sm">
       <CardContent className="flex gap-4 p-4">
-        {item.thumbnailUrl && (
-          <div
-            className="h-26 w-20 flex items-center justify-center
+        <Link to={`/product/${item.id}`}>
+          {item.thumbnailUrl && (
+            <div
+              className="h-26 w-20 flex items-center justify-center
       bg-[repeating-linear-gradient(90deg,#ffffff,#ffffff_4px,#e5e7eb_4px,#e5e7eb_8px)]"
-          >
-            <img
-              src={item.thumbnailUrl}
-              alt={item.name}
-              className="max-h-full max-w-full object-contain"
-            />
-          </div>
-        )}
+            >
+              <img
+                src={item.thumbnailUrl}
+                alt={item.name}
+                className="max-h-full max-w-full object-contain"
+              />
+            </div>
+          )}
+        </Link>
 
         <div className="flex flex-1 items-baseline justify-between">
           <div className="flex flex-col">
-            <p className="font-medium text-charcoal-black">{item.name}</p>
+            <Link to={`/product/${item.id}`} className="hover:underline">
+              <p className="font-medium text-charcoal-black">{item.name}</p>
+            </Link>
             <p className="text-sm text-graphite-gray">
               ${item.price.toFixed(2)}
             </p>
